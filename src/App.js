b1311211e0f9573759304154ac4579f04179af8f
Alejandro Mejia <alejomejiacuartas@outlook.com>
2019-06-20 13:01:13 -0500

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import logo from './images/logo.svg';
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <h1>Testing!</h1>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
