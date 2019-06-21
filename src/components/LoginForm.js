d87dce2290cfe65f089c6e020afad9d2b885d8b7
Alejandro Mejia <alejomejiacuartas@outlook.com>
2019-06-21 12:28:39 -0500

    Fix tags organization on Login view

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,21 +6,33 @@ class LoginForm extends Component {
 
   render(){
     return(
-        <form>
-          <div className="form-group">
-            <label htmlFor="username">Nombre de usuario</label>
-            <input type="text" className="form-control" id="username" autoComplete="username" required />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Contraseña</label>
-            <input type="password" className="form-control" id="password" autoComplete="current-password" required />
-          </div>
-          <div className="form-group form-check">
-            <input type="checkbox" className="form-check-input" id="remember" />
-            <label className="form-check-label" htmlFor="remember">Recuérdame</label>
-          </div>
-          <button type="submit" className="btn btn-primary btn-block border-0">Ingresar</button>
-        </form>
+      <form>
+        <div className="form-group">
+          <label htmlFor="username">Nombre de usuario</label>
+          <input 
+            id="username" 
+            className="form-control" 
+            type="text" 
+            placeholder="Ingrese su nombre de usuario"
+            autoComplete="username" 
+            required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Contraseña</label>
+          <input 
+            id="password"
+            className="form-control"
+            type="password" 
+            placeholder="Ingrese su contraseña"
+            autoComplete="current-password" 
+            required />
+        </div>
+        <div className="form-group form-check">
+          <input type="checkbox" className="form-check-input" id="remember" />
+          <label className="form-check-label" htmlFor="remember">Recuérdame</label>
+        </div>
+        <button type="submit" className="btn btn-primary btn-block border-0">Ingresar</button>
+      </form>
     )
   }
 
--- src/views/Login.js
+++ src/views/Login.js
@@ -6,33 +6,31 @@ import LoginForm from '../components/LoginForm';
 const LOGO_URL = 'https://www.insightcrime.org/wp-content/uploads/2017/07/logo.png';
 
 class Login extends Component{
-
-  componentDidMount(){
-    document.body.classList.add('login');
-  }
-
+  
   render(){
-
     return(
-      <div className="row h-100 align-items-center justify-content-center">
-        <div className="col-md-5 col-9">
-          <div className="mb-5">
-            <img className="logo mx-auto d-block" src={LOGO_URL} alt="logo" />
-          </div>
-          <div className="card mb-4">
-            <div className="card-body">
-              <LoginForm />
+      <div className="dark-theme">
+        <div className="row h-100 align-items-center justify-content-center">
+          <div className="col-md-5 col-9">
+            <div className="mb-5">
+              <img className="logo mx-auto d-block" src={LOGO_URL} alt="logo" />
             </div>
-          </div>
-          <div className="off-text">
-            <p className="mb-0">
-              <a className="text-secondary" href="/password-recovery.html">¿Olvidaste tu contraseña?</a>
-            </p> 
+            <div className="card mb-4">
+              <div className="card-body">
+                <LoginForm />
+              </div>
+            </div>
+            <div className="off-text">
+              <p className="mb-0">
+                <a className="text-secondary" href="/password-recovery.html">¿Olvidaste tu contraseña?</a>
+              </p> 
+            </div>
           </div>
         </div>
       </div>
     )
   }
+  
 }
 
 export default Login;
