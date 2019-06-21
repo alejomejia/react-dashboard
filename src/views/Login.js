8685d72eaa7a9e7f4eef7dc58b94f129d8f0f590
Alejandro Mejia <alejomejiacuartas@outlook.com>
2019-06-21 16:59:08 -0500

    Updating app logo

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 
+import LOGO from '../images/logo.png';
 import './style/Login.sass';
 import LoginForm from '../components/LoginForm';
 import PasswordRecoveryForm from '../components/PasswordRecoveryForm';
-
-const LOGO_URL = 'https://www.insightcrime.org/wp-content/uploads/2017/07/logo.png';
 
 class Login extends Component{
 
@@ -27,7 +26,7 @@ class Login extends Component{
         <div className="row h-100 align-items-center justify-content-center">
           <div className="col-md-5 col-9">
             <div className="mb-5">
-              <img className="logo mx-auto d-block" src={LOGO_URL} alt="logo" />
+              <img className="logo mx-auto d-block" src={LOGO} alt="logo" />
             </div>
             <div className="card mb-4">
               <div className="card-body">
